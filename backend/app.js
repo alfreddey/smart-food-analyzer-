d0bc7a1ea5ce3d40a7a1796a2f8431a7e572f232
alfreddey <126983853+alfreddey@users.noreply.github.com>
2025-07-23 21:11:12 -0200

    nothing important

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,6 @@ const ingredientsRouter = require("./routes/ingredients");
 const macrosRouter = require("./routes/macronutrients");
 
 const app = express();
-const tf = require("@tensorflow/tfjs-converter");
-const fs = require("node:fs");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -23,35 +21,5 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api", ingredientsRouter);
 app.use("/api", macrosRouter);
-
-const customIOHandler = {
-  load: async function () {
-    // Load and parse model.json
-    const modelJSON = JSON.parse(fs.readFileSync("./model/model.json", "utf8"));
-
-    // Load binary weight files listed in model.json
-    const weightDataBuffers = modelJSON.weightsManifest[0].paths.map((p) => {
-      const fullPath = path.join("./model/", p);
-      return fs.readFileSync(fullPath);
-    });
-
-    const weightData = Buffer.concat(weightDataBuffers);
-    // console.log(weightData);
-
-    return {
-      modelTopology: modelJSON.modelTopology,
-      weightSpecs: modelJSON.weightsManifest[0].weights,
-      weightData: weightData,
-    };
-  },
-};
-
-customIOHandler.load();
-
-async function foo() {
-  const model = await tf.loadGraphModel(customIOHandler);
-}
-
-foo();
 
 module.exports = app;
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,13 +1,87 @@
+"use client";
 import ImageDropzone from "@/components/customs/image-dropzone";
 import MacroDonutChart from "@/components/customs/macros";
 import { Button } from "@/components/ui/button";
+import * as tf from "@tensorflow/tfjs";
+import { useEffect, useRef } from "react";
 
 export default function Page() {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const run = async () => {
+      // Load model
+      const model = await tf.loadGraphModel("/model/model.json");
+      console.log("Model loaded");
+
+      const imageElement = imageRef.current;
+      if (!imageElement.complete) {
+        await new Promise((resolve) => {
+          imageElement.onload = resolve;
+        });
+      }
+
+      const imgTensor = tf.browser
+        .fromPixels(imageElement)
+        .resizeBilinear([224, 224])
+        .toFloat()
+        .div(127.5)
+        .sub(1)
+        .expandDims();
+
+      // Make prediction
+      const predictions = model.predict(imgTensor);
+      // predictions.print();
+      // const highestIndex = predictions.argMax(-1).arraySync()[0]; // Get top class
+      // console.log(imgTensor);
+      const labels = await loadLabels();
+      // console.log(labels[highestIndex]);
+
+      let topPred = await predictions.data();
+      topPred = [...topPred]
+        .map((predictions, i) => ({ i, predictions }))
+        .sort((a, b) => b.predictions - a.predictions)
+        .slice(0, 3);
+      console.log(topPred);
+
+      topPred.forEach(({ i, predictions }) => {
+        console.log(
+          `Label: ${labels[i]}, Confidence score ${(predictions * 100).toFixed(
+            2
+          )}%`
+        );
+      });
+    };
+
+    run();
+
+    const loadLabels = async function () {
+      const res = await fetch("/model/meta/labels.txt");
+      const text = await res.text();
+      const labels = text
+        .split("\n")
+        .map((line) => line.trim())
+        .filter(Boolean);
+      return labels;
+    };
+  }, []);
   return (
-    <div className="bg-[url(/bg-img.png)] bg-[#f4fcfc]">
+    <div className="bg-[url(/bg-img.png)] bg-[#f4fcfc];">
       <main className="mx-auto border border-t-0 bg-white px-10 shadow-2xl py-4 w-fit flex flex-col gap-12 min-h-screen">
         <div className="flex flex-col gap-4">
-          <h1 className="text-2xl/12 font-black tracking-wide">
+          <div>
+            <img
+              ref={imageRef}
+              src="/chicken_curry.jpg"
+              alt="Test Image"
+              crossOrigin="anonymous"
+              width={224}
+              height={224}
+              style={{ display: "none" }}
+            />
+          </div>
+
+          <h1 className="text-2xl/12 font-bold tracking-wide">
             Instant Food Facts - Just Take a Photo
           </h1>
           <p className="text-sm text-gray-500">See It. Snap It. Know It.</p>
